cb5af8798ceff714553e12fb99d958679583ea74
on14an_ <on14an@ON14AN>
2022-10-11 23:27:41 +0700

    Добавьте файлы проекта.

---
+++ 4zadanie/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+int[] mas = new int[100];
+Random rnd = new Random(DateTime.Now.Millisecond);
+
+int i;
+for (i = 0; i < mas.Length; i++)
+{
+    int x = rnd.Next(1001);
+    mas[i] = rnd.Next(2) == 0 ? x : -x;
+}
+
+Console.WriteLine("чётные числа:");
+for (i = 0; i < mas.Length; i++)
+{
+    if (mas[i] % 2 == 0)
+    {
+        Console.Write(mas[i] + ", ");
+    }
+}
+Console.WriteLine("\nчисла кратные 3");
+for (i = 0; i < mas.Length; i++)
+{
+    if (mas[i] % 3 == 0)
+    {
+        Console.Write(mas[i] + ", ");
+    }
+}
+Console.WriteLine("\nСумма всех отрицательных чисел");
+
+int count = 0;
+for (i = 0; i < mas.Length; i++)
+{
+    if (mas[i] < 0)
+    {
+        count += mas[i];
+    }
+}
+Console.Write(count);
